759cd311c2bb2f2b27a955c6fc82280c3a15d483
Brian Hannafious <brian@connectedsignals.com>
2020-04-23 12:15:52 -0700

    v0.3.0

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ with open("README.md") as fh:
 
 setup(
     name='bgzip',
-    version='0.2.2',
+    version='0.3.0',
     description='Utilities working with blocked gzip streams.',
     long_description=long_description,
     long_description_content_type='text/markdown',
