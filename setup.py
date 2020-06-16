adf26a78ce72682abf302143670abefdcff208bd
Brian Hannafious <brian@connectedsignals.com>
2020-06-16 13:40:21 -0700

    Add comments describing versioning

--- setup.py
+++ setup.py
@@ -36,11 +36,13 @@ with open("README.md") as fh:
 def get_version():
     filepath = os.path.join(os.path.dirname(__file__), "bgzip", "version.py")
     if os.path.isfile(filepath):
+        # Pick up the version packaged into a wheel or source distribution
         with open(filepath) as fh:
             version = dict()
             exec(fh.read().strip(), version)
             return version['__version__']
     else:
+        # Pick up version from git annotated tag (canonical version)
         p = subprocess.run(["git", "describe", "--tags", "--match", "v*.*.*"], stdout=subprocess.PIPE)
         p.check_returncode()
         out = p.stdout.decode("ascii").strip()
